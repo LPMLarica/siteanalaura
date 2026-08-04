692ac158cf772cd8468153141c26e51eb05930d1
Larissa Campos <larissacamposcardoso@gmail.com>
2026-08-04 12:46:18 -0300

    modificações

--- database/database.py
+++ database/database.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
-import config.settings
 from config.settings import settings
 
 engine = create_engine(
--- database/migrations.py
+++ database/migrations.py
@@ -1,7 +1,12 @@
-from database.database import Base
+import os
+import sys
+
+ROOT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if ROOT_DIR not in sys.path:
+    sys.path.insert(0, ROOT_DIR)
+
+from database.models import Base
 from database.database import engine
-import database.database
-import database.models
 
 
 def migrate():
